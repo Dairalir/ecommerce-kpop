a0591e304778a52692eb602d01e853f13d026bce
Dairalir <daimanvm@gmail.com>
2023-04-27 09:25:46 +0200

    modif rubriqueController pour recup la bdd

--- src/Controller/RubriqueController.php
+++ src/Controller/RubriqueController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\RubriqueRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +10,10 @@ use Symfony\Component\Routing\Annotation\Route;
 class RubriqueController extends AbstractController
 {
     #[Route('/rubrique', name: 'app_rubrique')]
-    public function index(): Response
+    public function index(RubriqueRepository $rubriqueRepository): Response
     {
         return $this->render('rubrique/index.html.twig', [
-            'controller_name' => 'RubriqueController',
+            'rubriques' => $rubriqueRepository->findAll(),
         ]);
     }
 }
