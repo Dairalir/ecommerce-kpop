464c2d4427a389179a9776b77f21c60e9dac41bd
Dairalir <daimanvm@gmail.com>
2023-05-02 23:08:50 +0200

    modif route controller

--- src/Controller/SousRubriqueController.php
+++ src/Controller/SousRubriqueController.php
@@ -7,9 +7,10 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[Route('/sous-rubrique', name: 'app_sous_rubrique')]
 class SousRubriqueController extends AbstractController
 {
-    #[Route('/sous-rubrique', name: 'app_sous_rubrique')]
+    #[Route('/', name: '')]
     //on injecte le repository dont on a besoin, on lui donne une variable.
     public function index(SousRubriqueRepository $sousRubriqueRepository): Response
     {
