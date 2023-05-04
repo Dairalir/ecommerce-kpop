b98434fd568a6467a761396f85ce9fa6285ba90d
Dairalir <daimanvm@gmail.com>
2023-05-04 10:39:37 +0200

    creation formulaire ajout produit

--- src/Controller/ProduitController.php
+++ src/Controller/ProduitController.php
@@ -3,10 +3,14 @@
 namespace App\Controller;
 
 use App\Entity\Produit;
+use App\Form\ProduitType;
 use App\Repository\ProduitRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\String\Slugger\SluggerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
 
 #[Route('/produit')]
 class ProduitController extends AbstractController
@@ -19,6 +23,47 @@ class ProduitController extends AbstractController
         ]);
     }
 
+    #[Route('/new', name: 'app_produit_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, ProduitRepository $produitRepository, SluggerInterface $slugger): Response
+    {
+        $produit = new Produit();
+        // création du formulaire
+        $form = $this->createForm(ProduitType::class, $produit);
+        // lecture du formulaire
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $produitRepository->save($produit, true);
+
+            $imgProduit = $form['picture']->getData();
+
+        if($imgProduit){
+            $originalFilename = pathinfo($imgProduit->getClientOriginalName(), PATHINFO_FILENAME);
+                // this is needed to safely include the file name as part of the URL
+                $safeFilename = $slugger->slug($originalFilename);
+                $newFilename = $safeFilename.'-'.uniqid().'.'.$imgProduit->guessExtension();
+
+                try {
+                    $imgProduit->move(
+                        $this->getParameter('img_product_directory'),
+                        $newFilename
+                    );
+                } catch (FileException $e) {
+                    // ... handle exception if something happens during file upload
+                }
+                $produit->setPicture($newFilename);
+            }
+
+            return $this->redirectToRoute('produit', [], Response::HTTP_SEE_OTHER);
+            
+        }
+
+        return $this->render('produit/new.html.twig', [
+                'produit' => $produit,
+                'form' => $form->createView(),
+            ]);
+    }
+
     #[Route('/{id}', name: 'app_produit_show')]
     // on utilise la classe de l'entité et on lui assigne une variable pour afficher le contenu de la classe
     public function show(Produit $produit):Response
@@ -27,4 +72,5 @@ class ProduitController extends AbstractController
             'produit' => $produit,
         ]);
     }
+
 }
---
+++ src/Form/ProduitType.php
@@ -0,0 +1,110 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Produit;
+use App\Entity\Fournisseur;
+use App\Entity\SousRubrique;
+use Doctrine\ORM\EntityRepository;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Validator\Constraints\Regex;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\MoneyType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+
+class ProduitType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+        ->add('name', TextType::class, [
+            'label' => 'Nom du produit',
+            'attr' => [
+                'placeholder' => 'Produit',
+            ],
+            'constraints' => [
+                new Regex([
+                    'pattern' => '/^[A-Za-zéèàçâêûîôäëüïö\_\-\s]+$/',
+                    'message' => 'Caratère(s) non valide(s)'
+                ]),
+            ]
+        ])
+        ->add('description', TextareaType::class, [
+            'label' => 'Description',
+            'attr' => [
+                'placeholder' => 'Description',
+            ],
+            'constraints' => [
+                new Regex([
+                    'pattern' => '/^[A-Za-zéèàçâêûîôäëüïö\_\-\s]+$/',
+                    'message' => 'Caratère(s) non valide(s)'
+                ]),
+            ]
+        ])
+            ->add('price', MoneyType::class, [
+                'label' => 'Prix',
+                'attr' => [
+                    'placeholder' => 'Prix',
+                ],
+                'constraints' => [
+                    new Regex([
+                        'pattern' => '/^\d+(,\d{1,2})?$/',
+                        'message' => 'Caratère(s) non valide(s)'
+                    ]),
+                ]
+            ])
+            ->add('picture', FileType::class,[
+                'label' => 'Image du produit',
+                'help' => 'Selectionnez une image',
+                'attr' => [
+                    'placeholder' => 'image.jpg',
+                ],
+            ])
+            ->add('stock', IntegerType::class,[
+                'label' => 'Stock du produit',
+                'attr' => [
+                    'placeholder' => 'Stock',
+                ],
+            ])
+            ->add('active', ChoiceType::class,[
+                'choices' => [
+                    'Activation du produit' => [
+                        'Oui' => 'active_yes',
+                        'Non' => 'active_no',
+                    ],
+                ],
+            ])
+            ->add('sous_rubrique', EntityType::class, [
+                'class' => SousRubrique::class,
+                'query_builder' => function (EntityRepository $er) {
+                    return $er->createQueryBuilder('sous_rubrique')
+                        ->orderBy('sous_rubrique.rubrique', 'ASC');
+                },
+                'choice_label' => 'name',
+                'multiple' => true,
+            ])
+            ->add('fournisseur', EntityType::class, [
+                'class' => Fournisseur::class,
+                'query_builder' => function (EntityRepository $er) {
+                    return $er->createQueryBuilder('fournisseur')
+                    ;
+                },
+                'choice_label' => 'name',
+                'multiple' => true,
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Produit::class,
+        ]);
+    }
+}
