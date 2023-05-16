46eef90e471abb29bdeff165ee2c58e040c809ff
Dairalir <daimanvm@gmail.com>
2023-05-16 14:41:12 +0200

    modif login et mise en page

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -53,8 +53,9 @@ class RegistrationController extends AbstractController
                     ->htmlTemplate('registration/confirmation_email.html.twig')
             );
             // do anything else you need here, like send an email
+        
 
-            return $this->redirectToRoute('app_index');
+            return $this->redirectToRoute('app_login');
         }
 
         return $this->render('registration/register.html.twig', [
