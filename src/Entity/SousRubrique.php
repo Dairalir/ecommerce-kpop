2f1d91042eb2f06a9a7d815a2f232ac434ceaffb
Dairalir <daimanvm@gmail.com>
2023-06-12 15:49:38 +0200

    api

--- src/Entity/SousRubrique.php
+++ src/Entity/SousRubrique.php
@@ -2,12 +2,14 @@
 
 namespace App\Entity;
 
-use App\Repository\SousRubriqueRepository;
-use Doctrine\Common\Collections\ArrayCollection;
-use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use ApiPlatform\Metadata\ApiResource;
+use App\Repository\SousRubriqueRepository;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\Common\Collections\ArrayCollection;
 
 #[ORM\Entity(repositoryClass: SousRubriqueRepository::class)]
+#[ApiResource()]
 class SousRubrique
 {
     #[ORM\Id]
